6c6a123ba5a4697b80c0accf41bbf704796a1e0d
M4ximumPizza <MaximumUltimateDestroyer@gmail.com>
2024-04-07 22:16:06 -0700

    Deleted useless files

--- src/main/java/mi/m4x/project/circuitverse/Main.java
+++
@@ -1,8 +0,0 @@
-package mi.m4x.project.circuitverse;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Booting Game");
-        CircuitVerse.instance = new CircuitVerse();
-    }
-}
